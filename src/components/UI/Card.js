93b43bbc25593dfdeaf8736f9d46d7259e22ffac
pgreber2504 <p.greber2504@gmail.com>
2022-05-22 15:00:09 +0200

    init added basic components

---
+++ src/components/UI/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Card.module.css';
+
+const Card = (props) => {
+    const allClasses = `${classes.card} ${props.className}`
+
+    return (
+        <div className={allClasses}>{props.children}</div>
+    )
+}
+
+export default Card
---
+++ src/components/UI/LoadSpinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from 'LoadSpinner.module.css'
+
+const LoadSpinner = () => {
+    return (
+        <div className={classes['load-spinner']}></div>
+    )
+}
+
+export default LoadSpinner
