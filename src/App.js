70d2d9c0770c59b0e732434c1dbc06e666f2d733
pgreber2504 <p.greber2504@gmail.com>
2022-05-23 13:41:30 +0200

    added lazy loading optimization

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import NotFound from './pages/NotFound';
-import SingleQuote from './pages/SingleQuote';
+import LoadSpinner from './components/UI/LoadSpinner';
+
+const SingleQuote = React.lazy(() => import('./pages/SingleQuote'));
+const NewQuote = React.lazy(() => import('./pages/NewQuote'))
+const NotFound = React.lazy(() => import('./pages/NotFound'))
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'))
 
 const App = () => {
     return (
         <Layout>
-            <Routes>
-                <Route path='/' element={<Navigate to='/quotes' />} />
-                <Route path='/quotes' element={<AllQuotes />} />
-                <Route path='/quotes/:quoteId/*' element={<SingleQuote />} />
-                <Route path='/new-quote' element={<NewQuote />} />
-                <Route from='/*' element={<NotFound />} />
-            </Routes>
+            <Suspense fallback={
+                <div className='centered'>
+                    <LoadSpinner />
+                </div>
+            }>
+                <Routes>
+                    <Route path='/' element={<Navigate to='/quotes' />} />
+                    <Route path='/quotes' element={<AllQuotes />} />
+                    <Route path='/quotes/:quoteId/*' element={<SingleQuote />} />
+                    <Route path='/new-quote' element={<NewQuote />} />
+                    <Route from='/*' element={<NotFound />} />
+                </Routes>
+            </Suspense>
         </Layout>
     )
 }
